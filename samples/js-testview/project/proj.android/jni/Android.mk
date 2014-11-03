3071249d0e312ec68d959b3d4973d2b350a1aac7
wuzhiming <wuzhiming.chukong@gmail.com>
2014-11-03 16:35:26 +0800

    add view demo

---
+++ samples/js-testview/project/proj.android/jni/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := js_tests_shared
+
+LOCAL_MODULE_FILENAME := libcocos2djs
+
+LOCAL_SRC_FILES := main.cpp \
+                   ../../Classes/AppDelegate.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+LOCAL_STATIC_LIBRARIES := cocos_jsb_static
+LOCAL_STATIC_LIBRARIES += jsb_pluginx_static
+
+
+LOCAL_EXPORT_CFLAGS := -DCOCOS2D_DEBUG=1
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,bindings) 
+$(call import-module,cocos2d-x/plugin/jsbindings)
